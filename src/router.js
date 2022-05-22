179db43075c0e79beb51bbf08556499517bcb251
ruanjiayou <1439120442@qq.com>
2022-05-22 15:40:10 +0800

    系统兼容处理

--- src/router.js
+++ src/router.js
@@ -7,7 +7,7 @@ const routeDir = path.join(__dirname, 'routes')
 
 loader({ dir: routeDir, recusive: true }, info => {
   const subRouter = require(info.fullpath);
-  const route = path.relative(routeDir, info.dir)
+  const route = path.relative(routeDir, info.dir).replace(path.sep, '/')
   if (subRouter) {
     router.use(`/${route}`, subRouter.routes())
   }
